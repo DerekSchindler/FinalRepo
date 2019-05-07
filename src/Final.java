7415c337f061c8d6e144372d67553571fac7c416
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-05-07 07:57:47 -0400

    Created project

---
+++ src/Final.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Final {
+    public static void main(String[]args){
+        Scanner equinox;
+        equinox = new Scanner(System.in);
+        String user, comp;
+        System.out.println("Welcome to Rock Paper Scissors.\n  ");
+        while(true){
+            
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+}
