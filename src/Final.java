fbb9f09431eadea6af5f4049892ca4c7782efbb1
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-05-09 07:43:12 -0400

    Added scanner for user input and converted user input to uppercase

--- src/Final.java
+++ src/Final.java
@@ -8,13 +8,30 @@ public class Final {
         System.out.println("Welcome to Rock Paper Scissors.\n  ");
         while(true) {
             String[] wordArray = {"Rock", "Paper", "Scissors"};
-
             int number = (int)(Math.random()*100);
+           comp = comInput(number);
+           System.out.println("I already have my play. Please enter your play.");
+           user=equinox.nextLine();
+           user=user.toUpperCase();
+           System.out.println(user);
 
 
         }
 
 
+
+    }
+    public static String comInput(int number){
+
+        if(number<34){
+            return "ROCK";
+        }
+        else if(number>65){
+            return "PAPER";
+        }
+        else{
+            return "SCISSORS";
+        }
 
     }
 
