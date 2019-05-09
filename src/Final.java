60060fe7559b878e258b7fd9548d39cc16a05db1
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-05-09 08:22:11 -0400

    Fixed a bug in the score keeping meachanism

--- src/Final.java
+++ src/Final.java
@@ -5,7 +5,6 @@ public class Final {
         Scanner equinox;
         equinox = new Scanner(System.in);
         String user, comp;
-        int victory; int compScore=0; int userScore=0;
         System.out.println("Welcome to Rock Paper Scissors.\n  ");
         while(true) {
             String[] wordArray = {"Rock", "Paper", "Scissors"};
@@ -15,22 +14,10 @@ public class Final {
            user=equinox.nextLine();
            user=user.toUpperCase();
            System.out.println("My play was "+comp+" your play was "+user);
-           victory = outcome(comp, user);
-           if(victory==1){
-               compScore=compScore++;
-           }
-           if(victory==2){
-               userScore=userScore++;
-           }
-           System.out.println("My score is "+compScore+". Your score is "+userScore+".");
-           System.out.println("Lets play again. \n  \n  \n  \n  ");
-
-
+           outcome(comp, user);
+           System.out.println("Lets play again. \n \n \n \n \n ");
         }
-
-
-
-    }
+        }
     public static String comInput(int number){
 
         if(number<34){
@@ -44,36 +31,37 @@ public class Final {
         }
 
     }
-    public static int outcome(String comp, String user){
+    public static void outcome(String comp, String user){
+        int compScore=0; int userScore=0;
+
         if(comp.equals(user)){
             System.out.println("Its a tie!");
-            return 0;
         }
         else if(user.equals("ROCK")&&comp.equals("PAPER")){
             System.out.println("Paper beats rock. I win!");
-            return 1;
+           compScore=compScore+1;
         }
         else if(user.equals("ROCK")&&comp.equals("SCISSORS")){
             System.out.println("Rock beats scissors. You win!");
-            return 2;
+            userScore=userScore+1;
         }
         else if(user.equals("PAPER")&&comp.equals("SCISSORS")){
             System.out.println("Scissors beats paper. I win!");
-            return 1;
+            compScore=compScore +1;
         }
         else if(user.equals("PAPER")&&comp.equals("ROCK")){
             System.out.println("Paper beats rock. You win!");
-            return 2;
+            userScore=userScore+1;
         }
         else if(user.equals("SCISSORS")&&comp.equals("ROCK")){
             System.out.println("Rock beats scissors. I win!");
-            return 1;
+            compScore=compScore+1;
         }
         else if(user.equals("SCISSORS")&&comp.equals("PAPER")){
             System.out.println("Scissors beats paper. You win!");
-            return 2;
+            userScore=userScore+1;
         }
-        return 0;
+        System.out.println("My current score is "+compScore+". Your current score is "+userScore+".");
     }
 
 }
