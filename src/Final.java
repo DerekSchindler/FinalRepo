4ee24ea24f1fe09cf8499297269019b72b04030f
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-05-09 07:56:43 -0400

    Found who would win with method

--- src/Final.java
+++ src/Final.java
@@ -13,7 +13,8 @@ public class Final {
            System.out.println("I already have my play. Please enter your play.");
            user=equinox.nextLine();
            user=user.toUpperCase();
-           System.out.println(user);
+           System.out.println("My play was "+comp+" your play was "+user);
+           outcome(comp, user);
 
 
         }
@@ -32,6 +33,41 @@ public class Final {
         else{
             return "SCISSORS";
         }
+
+    }
+    public static int outcome(String comp, String user){
+        if(comp.equals(user)){
+            System.out.println("Its a tie!");
+            return 0;
+        }
+        else if(user.equals("ROCK")&&comp.equals("PAPER")){
+            System.out.println("Paper beats rock. I win!");
+            return 1;
+        }
+        else if(user.equals("ROCK")&&comp.equals("SCISSORS")){
+            System.out.println("Rock beats scissors. You win!");
+            return 2;
+        }
+        else if(user.equals("PAPER")&&comp.equals("SCISSORS")){
+            System.out.println("Scissors beats paper. I win!");
+            return 1;
+        }
+        else if(user.equals("PAPER")&&comp.equals("ROCK")){
+            System.out.println("Paper beats rock. You win!");
+            return 2;
+        }
+        else if(user.equals("SCISSORS")&&comp.equals("ROCK")){
+            System.out.println("Rock beats scissors. I win!");
+            return 1;
+        }
+        else if(user.equals("SCISSORS")&&comp.equals("PAPER")){
+            System.out.println("Scissors beats paper. You win!");
+            return 2;
+        }
+
+
+
+
 
     }
 
